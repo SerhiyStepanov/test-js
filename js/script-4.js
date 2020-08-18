1004f56e17fbb15208a4b51e8660420a376bdccb
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-08-18 10:11:06 +0300

    src-5js

--- js/script-4.js
+++ js/script-4.js
@@ -111,24 +111,24 @@
 
 /*   4   */
 
-const account = {
-  owner: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["order-1", "order-2", "order-3"],
-  changeDiscount(value) {
-    discount = value; // Write code in this line
-  },
-  showOrders() {
-    return orders; // Write code in this line
-  },
-  addOrder(cost, order) {
-    balance -= cost; // Write code in this line
-    orders.push(order); // Write code in this line
-  },
-};
-const copyAccount = Object.assign({}, account);
-copyAccount.orders = [...account.orders];
+// const account = {
+//   owner: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["order-1", "order-2", "order-3"],
+//   changeDiscount(value) {
+//     discount = value; // Write code in this line
+//   },
+//   showOrders() {
+//     return orders; // Write code in this line
+//   },
+//   addOrder(cost, order) {
+//     balance -= cost; // Write code in this line
+//     orders.push(order); // Write code in this line
+//   },
+// };
+// const copyAccount = Object.assign({}, account);
+// copyAccount.orders = [...account.orders];
 // копируем для автотестов ссылочные типы
 
 /*
