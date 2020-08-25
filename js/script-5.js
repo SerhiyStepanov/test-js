c64ed3ca5b56f36b2fd42c220f59908b1c18ad3e
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-08-25 23:28:51 +0300

    5-5

--- js/script-5.js
+++ js/script-5.js
@@ -132,31 +132,166 @@
  */
 
 // Write code under this line
-class StringBuilder {
-  constructor(value) {
-    this._value = value;
+// class StringBuilder {
+//   constructor(value) {
+//     this.value = value;
+//   }
+//   get(value) {
+//     this.value = value;
+//   }
+//   append(str) {
+//     this.value = this.value + str;
+//   }
+//   prepend(str) {
+//     this.value = str + this.value;
+//   }
+//   pad(str) {
+//     return str + this.append(str) + (this.prepend(str) + str);
+//   }
+// }
+
+// console.log(typeof StringBuilder);
+// // 'function'
+
+// const builder = new StringBuilder(".");
+
+// builder.append("^");
+// console.log(builder.value); // '.^'
+
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
+
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
+
+/*Задача 5-5
+класс
+
+Напиши класс Car с указанными свойствами и методами.
+
+class Car {
+  /*
+   * Добавь `статический` метод 
+   * `getSpecs(car)`, который принимает 
+   * объект-машину как параметр 
+   * и возвращает шаблонную строку 
+   * со свойствами и значениями объекта.
+   * Свойства: 
+   *   maxSpeed, 
+   *   speed, 
+   *   isOn, 
+   *   distance,
+   *   price
+   * Пример строки, полученной этим методом:
+   * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+   */
+
+/*
+ * Конструктор получает объект настроек.
+ *
+ * Добавь свойства будущеего экземпляра класса:
+ *  speed - текущая скорость,
+ *          начальное значение `0`
+ *  price - цена автомобиля
+ *  maxSpeed - максимальная скорость
+ *  isOn - заведен ли автомобиль.
+ *         Значения `true` или `false`,
+ *         начальное значение false
+ *  distance - пробег в километрах,
+ *             начальное значение `0`
+ */
+// constructor() { }
+
+/*
+ * Добавь геттер и сеттер
+ * для свойства `price`, который будет
+ * работать с свойством цены автомобиля.
+ *
+ * ВАЖНО: для записи методов get и set
+ * значение параметра записывают с
+ * подчеркиванием. См. ниже пример 1.
+ */
+
+/*
+ * Метод, который заводит автомобиль
+ * Записывает в свойство `isOn` значение `true`
+ */
+// turnOn() { }
+
+/*
+ * Метод, чтобы заглушить автомобиль
+ * Этот метод должен записывать
+ * в свойство isOn значение false,
+ * и сбрасывать текущую скорость до 0
+ */
+// turnOff() { }
+
+/*
+ * Этот метод должен добавлять
+ * к свойству `speed` полученное
+ * значение, при условии,
+ * что результирующая скорость
+ * не больше чем значение свойства `maxSpeed`
+ */
+// accelerate(value) { }
+
+/*
+ * Этот метод должен отнимать
+ * от свойства `speed`
+ * полученное значение, при условии,
+ * что результирующая скорость не меньше 0
+ */
+
+// decelerate(value) { }
+
+/*
+ * Этот метод должен добавлять к свойству
+ * `distance` пробег в километрах,
+ * т.е. hours * speed,
+ * но только в том случае,
+ * если машина заведена!
+ */
+// drive(hours) { }
+// }
+
+// #### Пример 1.
+
+//   ```js
+// class Guest {
+//   // Собственные свойства класса размещаем в конструкторе
+//   constructor(name, roomNumber) {
+//     /* параметр name не должен
+//      *   совпадать с названием свойства
+//      *   но должен быть похож.
+//      *   Поэтому используют прием смены
+//      *   названия добавляя символ '_'.
+//      *   Хотя это может любой другой символ
+//      */
+//     this._name = name;
+//   }
+
+//   // Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+//   get name() {
+//     return this._name;
+//   }
+
+//   set name(value) {
+//     this._name = value;
+//   }
+// }
+
+// */
+
+class Car {
+  getSpecs(car) {}
+
+  constructor(config = {}) {
+    this._maxSpeed = maxSpeed;
+    this._speed = speed;
+    this._isOn = isOn;
+    this._distance = distance;
+    this._price = price;
   }
-  getValue(value) {
-    this.value = _value;
-  }
-  append(str) {
-    this.value = this._value + str;
-  }
-  prepend(str) {
-    this.value = str + this._value + str;
-  }
-  pad(str) {}
 }
-console.log(typeof StringBuilder);
-// 'function'
 
-const builder = new StringBuilder(".");
-
-builder.append("^");
-console.log(builder.value); // '.^'
-
-builder.prepend("^");
-console.log(builder.value); // '^.^'
-
-builder.pad("=");
-console.log(builder.value); // '=^.^='
+console.log(Car);
