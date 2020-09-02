ed352f098e1c84e52cd31e64de4e1d8f263f0995
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-02 20:50:01 +0300

    6-10

--- js/script-6.js
+++ js/script-6.js
@@ -365,22 +365,22 @@ reduce, filter, sort
  */
 
 // Write code under this line
-const getSortedUniqueSkills = (array) =>
-  array
-    .reduce((acc, { skills }) => acc.concat(skills), [])
-    .filter((skills, index, array) => {
-      return array.indexOf(skills) === index;
-    })
-    .sort();
+// const getSortedUniqueSkills = (array) =>
+//   array
+//     .reduce((acc, { skills }) => acc.concat(skills), [])
+//     .filter((skills, index, array) => {
+//       return array.indexOf(skills) === index;
+//     })
+//     .sort();
 
-console.log(getSortedUniqueSkills(users));
-/* [ 'adipisicing', 'amet',
- 'anim', 'commodo',
- 'culpa', 'elit',
- 'ex', 'ipsum',
- 'irure', 'laborum',
- 'lorem', 'mollit',
- 'non', 'nostrud',
- 'nulla', 'proident',
- 'tempor', 'velit',
- 'veniam' ]; */
+// console.log(getSortedUniqueSkills(users));
+// /* [ 'adipisicing', 'amet',
+//  'anim', 'commodo',
+//  'culpa', 'elit',
+//  'ex', 'ipsum',
+//  'irure', 'laborum',
+//  'lorem', 'mollit',
+//  'non', 'nostrud',
+//  'nulla', 'proident',
+//  'tempor', 'velit',
+//  'veniam' ]; */
