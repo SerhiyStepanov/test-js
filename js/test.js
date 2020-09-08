65042a58eb32fca148939bcfba3a4b503c36f716
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-08 10:28:59 +0300

    cart

--- js/test.js
+++ js/test.js
@@ -1,62 +1,31 @@
-// const cart = {
-//   items: [],
+const cart = {
+  items: [],
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    this.items.push(product);
+  },
+  remove(productName) {},
+  clear() {
+    return (this.items = []);
+  },
+  countTotalPrice() {},
+};
 
-//   getItems() {
-//     return this.items;
-//   },
+cart.add({ name: "apple", price: 50 });
+cart.add({ name: "apple", price: 50 });
+cart.add({ name: "lemon", price: 60 });
+cart.add({ name: "banan", price: 100 });
+cart.add({ name: "apple", price: 50 });
+cart.add({ name: "lemon", price: 60 });
+cart.add({ name: "banan", price: 100 });
+cart.add({ name: "banan", price: 100 });
+cart.add({ name: "banan", price: 100 });
 
-//   add(product) {
-//     for (const item of this.items) {
-//       console.log(item);
-//       if (item.name === product.name) {
-//         item.quantity += 1;
-//         return;
-//       }
-//     }
-//     const newProduct = { ...product, quantity: 1 };
-//     this.items.push(newProduct);
-//   },
-
-//   remove(productName) {
-//     // const { items } = this;
-//     for (let i = 0; i < this.items.length; i += 1) {
-//       const item = this.items[i];
-//       if (productName === item.name) {
-//         this.items.splice(i, 1);
-//       }
-//     }
-//   },
-
-//   clear() {
-//     return (this.items = []);
-//   },
-
-//   countTotalPrice() {
-//     let total = 0;
-//     for (const { price, quantity } of this.items) {
-//       total += price * quantity;
-//     }
-//     return total;
-//   },
-//   increaseQuantity(productName) {},
-//   decreaseQuantity(productName) {},
-// };
-
-// cart.add({ name: "apple", price: 50 });
-// cart.add({ name: "apple", price: 50 });
-// cart.add({ name: "lemon", price: 60 });
-// cart.add({ name: "banan", price: 100 });
-// cart.add({ name: "apple", price: 50 });
-// cart.add({ name: "lemon", price: 60 });
-// cart.add({ name: "banan", price: 100 });
-// cart.add({ name: "banan", price: 100 });
-// cart.add({ name: "banan", price: 100 });
-
-// // console.table(cart.getItems());
-// // cart.remove("lemon");
-// // console.log(cart.getItems());
-// // // cart.clear();
-// // // console.log(cart.getItems());
-// // console.log(cart.countTotalPrice());
-
-// ------------
+console.table(cart.getItems());
+cart.remove("lemon");
+console.log(cart.getItems());
+cart.clear();
+console.log(cart.getItems());
+// console.log(cart.countTotalPrice());
