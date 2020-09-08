7f166691f5c5e109b8ced754f0cdfe5f53937059
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-08 14:59:23 +0300

    object

--- js/test.js
+++ js/test.js
@@ -1,31 +1,62 @@
 const cart = {
   items: [],
+
   getItems() {
     return this.items;
   },
+
   add(product) {
-    this.items.push(product);
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quantity += 1;
+        return;
+      }
+    }
+    const newProduct = { ...product, quantity: 1 };
+
+    this.items.push(newProduct);
   },
-  remove(productName) {},
+
+  remove(productName) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      // console.log(this.items[i]);
+      if (this.items[i].name === productName) {
+        this.items.splice(i, 1);
+      }
+    }
+  },
+
   clear() {
     return (this.items = []);
   },
-  countTotalPrice() {},
+
+  countTotalPrice() {
+    let total = 0;
+    for (const { price, quantity } of this.items) {
+      // console.log(item);
+      total += price * quantity;
+    }
+    return total;
+  },
 };
 
-cart.add({ name: "apple", price: 50 });
-cart.add({ name: "apple", price: 50 });
-cart.add({ name: "lemon", price: 60 });
-cart.add({ name: "banan", price: 100 });
+cart.add({ name: "chees", price: 30 });
 cart.add({ name: "apple", price: 50 });
 cart.add({ name: "lemon", price: 60 });
-cart.add({ name: "banan", price: 100 });
-cart.add({ name: "banan", price: 100 });
-cart.add({ name: "banan", price: 100 });
+cart.add({ name: "chees", price: 30 });
+cart.add({ name: "apple", price: 50 });
+cart.add({ name: "lemon", price: 60 });
+cart.add({ name: "chees", price: 30 });
+cart.add({ name: "banan", price: 20 });
+cart.add({ name: "chees", price: 30 });
+cart.add({ name: "chees", price: 30 });
+cart.add({ name: "cuconut", price: 75 });
 
 console.table(cart.getItems());
-cart.remove("lemon");
-console.log(cart.getItems());
-cart.clear();
-console.log(cart.getItems());
-// console.log(cart.countTotalPrice());
+
+// cart.getItems();
+// cart.remove(productName);
+// cart.clear();
+console.log(`Total price : ${cart.countTotalPrice()} $`);
+// console.log(cart.getItems());
+// console.log(cart);
