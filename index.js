5eefe870edc92e3011ca355df39bf27d7ba790eb
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-20 13:10:54 +0200

    add button go to top

--- index.js
+++ index.js
@@ -24,16 +24,16 @@
 // BUTTON
 let view = {
   setUpEventListener: function () {
-    this.displayScroll();
-    this.goToTopClick();
+    this.displayScroll(); //Display Scroll Button after specific height.
+    this.goToTopClick(); //Onclick Go to top function.
   },
 
   displayScroll: function () {
     let goToTop = document.getElementById("goToTop");
     window.addEventListener("scroll", () => {
-      let offSetY = window.scrollY;
-      let viewHeight = window.innerHeight;
-      viewHeight /= 2;
+      let offSetY = window.scrollY; // Scroll Y Pos.
+      let viewHeight = window.innerHeight; // Size of the Browser Window
+      viewHeight /= 2; // Divide viewHeight
 
       if (offSetY >= viewHeight) {
         goToTop.style.display = "block";
