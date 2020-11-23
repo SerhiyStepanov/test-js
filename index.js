78fbd7bf43213914178a740aebd7e23dae7ce4a6
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-23 21:50:42 +0200

    ok

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
-import onFooterBtnClick from "./js/basicLightbox";
-import view from "./js/goToTop";
+// import onFooterBtnClick from "./js/basicLightbox";
+// import view from "./js/goToTop";
 
-const btnNav = document.querySelector(".button-navigation");
+const { date } = require("joi");
 
-const btnLeft = document.querySelector('button[data-action="left"]');
-const btnStart = document.querySelector('button[data-action="start"]');
+// const btnNav = document.querySelector(".button-navigation");
 
-const btnPrevious = document.querySelector('button[data-action="previous"]');
-const btnBetween = document.querySelector('button[data-action="between"]');
-const btnNext = document.querySelector('button[data-action="next"]');
+// const btnLeft = document.querySelector('button[data-action="left"]');
+// const btnStart = document.querySelector('button[data-action="start"]');
 
-const btnEnd = document.querySelector('button[data-action="end"]');
-const btnRight = document.querySelector('button[data-action="right"]');
+// const btnPrevious = document.querySelector('button[data-action="previous"]');
+// const btnBetween = document.querySelector('button[data-action="between"]');
+// const btnNext = document.querySelector('button[data-action="next"]');
 
-console.log(btnLeft);
-console.log(btnStart);
+// const btnEnd = document.querySelector('button[data-action="end"]');
+// const btnRight = document.querySelector('button[data-action="right"]');
 
-console.log(btnPrevious);
-console.log(btnBetween);
-console.log(btnNext);
+// console.log(btnLeft);
+// console.log(btnStart);
 
-console.log(btnEnd);
-console.log(btnRight);
-const API_KEY = "9fba788361f0940b39e64c54ec217196";
+// console.log(btnPrevious);
+// console.log(btnBetween);
+// console.log(btnNext);
 
-const url = `https:api.themoviedb.org/3/movie/lists?api_key=${API_KEY}`;
+// console.log(btnEnd);
+// console.log(btnRight);
+// const API_KEY = "9fba788361f0940b39e64c54ec217196";
 
-fetch(url)
-  .then((res) => res.json)
-  .then((data) => {
-    return data;
-  });
+// const url = `https:api.themoviedb.org/3/movie/lists?api_key=${API_KEY}`;
 
-btnLeft.addEventListener("click", onBtnClickLeft);
-btnStart.addEventListener("click", onBtnClickStart);
-btnPrevious.addEventListener("click", onBtnClickPrevious);
-btnBetween.addEventListener("click", onBtnCliclBetween);
-btnNext.addEventListener("click", onBtnClickNext);
-btnEnd.addEventListener("click", onBtnClickEnd);
-btnRight.addEventListener("click", onBtnClickRight);
+// fetch(url)
+//   .then((res) => res.json)
+//   .then((data) => {
+//     return data;
+//   });
 
-function onBtnClickLeft(event) {
-  console.log(event.currentTarget);
-}
-function onBtnClickStart(event) {
-  console.log(event.currentTarget);
-}
-function onBtnClickPrevious(event) {
-  console.log(event.currentTarget);
-}
-function onBtnCliclBetween(event) {
-  console.log(event.currentTarget);
-}
-function onBtnClickNext(event) {
-  console.log(event.currentTarget);
-}
-function onBtnClickEnd(event) {
-  console.log(event.currentTarget);
-}
-function onBtnClickRight(event) {
-  console.log(event.currentTarget);
-}
+// btnLeft.addEventListener("click", onBtnClickLeft);
+// btnStart.addEventListener("click", onBtnClickStart);
+// btnPrevious.addEventListener("click", onBtnClickPrevious);
+// btnBetween.addEventListener("click", onBtnCliclBetween);
+// btnNext.addEventListener("click", onBtnClickNext);
+// btnEnd.addEventListener("click", onBtnClickEnd);
+// btnRight.addEventListener("click", onBtnClickRight);
+
+// function onBtnClickLeft(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnClickStart(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnClickPrevious(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnCliclBetween(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnClickNext(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnClickEnd(event) {
+//   console.log(event.currentTarget);
+// }
+// function onBtnClickRight(event) {
+//   console.log(event.currentTarget);
+// }
